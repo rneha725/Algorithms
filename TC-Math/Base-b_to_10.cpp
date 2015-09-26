d64213d078d8ba91053793be4bfccf96e0837e0a
Neha <rneha725@gmail.com>
2015-09-26 18:57:56 +0530

    Created Base-b_to_10.cpp

---
+++ TC-Math/Base-b_to_10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int convertInDec(int n, int b)
+{
+	int result = 0, multiplier = 1;
+	while(n)
+	{
+		result = result + n%10*multiplier;
+		multiplier = multiplier*b;
+		n=n/10;
+	}
+	return result;
+}
+
+using namespace std;
+
+int main()
+{
+	int num, base;
+	cin>>num>>base;
+	cout<<convertInDec(n, base);
+}
